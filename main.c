f39fa3da7144edce70f040efb0c4af896fe17fe6
Shubhang Srikoti <70000106+shubhangsrikoti@users.noreply.github.com>
2020-09-18 01:18:21 +0000

    .

--- main.c
+++ main.c
@@ -1,39 +1,37 @@
 // Author: Shubhang Srikoti svs6959@psu.edu
-// Collaborator:
-// Collaborator:
-// Collaborator:
+// Collaborator: Geng Niu gjn5124@psu.edu
+// Collaborator: Max Skeen mls6984@psu.edu
+// Collaborator: Ananya Ashwinikumar ava6290@psu.edu
 // Section: 4
 // Breakout Room: 7
-
+#include <readline/readline.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <readline/readline.h>
 
 int sum_n(int n) {
-  
-  if (n >= 1) 
-    return n + sum_n(n - 1);
-  
-  else
+  if (n == 0) {
     return 0;
+  }
+  else {
+    return n + sum_n(n - 1);
+  }
 }
 
-int print_n(const char *s, int n) {
-  if (n >= 1) {
-    printf("%s\n", s);
-    return print_n(s, n - 1);
+void print_n(const char *s, int n) {
+  if(n == 0) {
+    return;
   }
-  else
-    return 0;
+  else {
+    printf("%s\n", s);
+    print_n(s, n - 1);
+  }
 }
 
 int main(void) {
-
-  int num = atoi(readline("Enter an int: "));
-  printf("sum is %i\n", sum_n(num));
-
+  int n;
+  char *integer = readline("Enter an int: ");
+  n = atoi(integer);
+  printf("sum is %d.\n", sum_n(n));
   char *string = readline("Enter a string: ");
-  print_n(string, num);
-
-  return 0;
+  print_n(string, n);
 }
